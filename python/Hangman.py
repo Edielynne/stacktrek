10290457ea72003f52385004afbe3c95bda27ecb
Edielynne <barnesedielynne171999@gmail.com>
2022-10-02 01:41:38 +0800

    Python Project Lesson 5

---
+++ python/Hangman.py
@@ -0,0 +1,113 @@
+import time
+import random
+import threading
+
+def countdown():
+    global timer 
+    global win 
+    global wrong
+    win = 0
+    timer= 5
+    wrong = 0
+    while timer:  
+        mins, secs = divmod(timer, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+       #print(timeformat, end='\r')
+        time.sleep(1)
+        timer -= 1
+    if win == 1:
+        print("Congratulations")
+    elif wrong >4 :
+        print("Game Over Wrong answer")
+    else:
+        print("Time out!")
+    
+word = ["Hello","Word","Yes", "No"]
+used = []
+start = ""
+
+
+#randmom picking of array 
+rand = random.randint(0,len(word)-1) 
+guess = word[rand].lower() #word
+
+#declare staring blanks
+for i in range(len(guess)):
+    start += "_"
+
+#Start the game 
+game = True
+count_thread = threading.Thread(target = countdown) 
+while game == True:
+    play = input("Start? (Y/N) ") 
+    if(play.lower() == "y" ):
+        count_thread.start()
+        game = False
+
+    elif(play.lower()== "n"):
+        print("Bye")
+        game = False
+        exit()
+    else:
+        print("Press Y to Continue || Press N to Close")
+
+
+
+guess_array = [*guess] #[y,e,s]
+blank = [*start] #[y,-,-] y__
+print(f"{len(guess)} Letter Word")
+print(start)
+ 
+while timer > 0 :
+    unlockedletter = "".join(blank) # yes || guess = yes
+
+    if(wrong >4):
+        timer=1
+        count_thread.join()
+        breaky
+    
+    if(unlockedletter == guess ): 
+        timer = 1
+        win = 1
+        count_thread.join()
+        break
+    
+    
+    print(f"Wrong: {wrong}")
+    user_answer = input("Your Guess: \n").lower()
+
+    if(user_answer.lower() in used):
+      print(f"{user_answer} is already given") 
+      continue
+     
+
+    if(user_answer in guess_array):
+        used.append(user_answer)
+        for i in range(len(guess)):
+            if guess_array[i].lower() == user_answer:
+                 del(blank[i])
+                 blank.insert(i, guess_array[i])         
+        print("".join(blank))
+
+    else:
+        wrong+=1
+
+    used.append(user_answer)
+
+
+
+
+
+
+       
+        
+
+
+
+
+        
+
+
+
+
+
--- python/python/Quiz Game.py
+++ python/python/Quiz Game.py
@@ -1,12 +1,31 @@
-question = [ ]
-answer = [""]
+
+
+
+question = ["Who is the father of modern computer?", "Who created linux operating system?", "What is the most common programming language?", "What kind of control structure will you use to determine/validate a specific answer?", ]
+answer = ["b", "a", "c", "a"]
+choices = ["A. Mark zuckerberg, B. Charles babbage, C. Will smith", "A. Linus torvalds, B. Gabe newell, C. Steve jobs", "A. html/css, B. python, C. javascript", "A. if else, B. try/catch, C. while loop"]
 score = 0
 
 for i in range (len(question)):
     print(f"Question {i+1} : {question[i]}")
-    user_answer = input("Your answer: ")
+    print("-----------")
+    print(f"Choices:" )
+    c = choices[i].split(", ")
+    
+    for y in range(len(c)):
+        print(c[y]+"\n")
 
-    if answer [i] == user_answer:
+    ans = True
+    while ans == True:
+        user_answer= input("Your answer: ")
+        if user_answer.lower() == "a" or user_answer.lower() =="b" or user_answer.lower() =="c":
+            ans = False
+        else:
+            print(f"{user_answer} is not in the choices")
+            continue
+    
+
+    if answer [i].lower() == user_answer.lower():
         print ("Correct!")
         score += 1
 
